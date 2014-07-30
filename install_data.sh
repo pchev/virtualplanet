1176a7529bd918a72593cfc8370a8e5b48305c65
Patrick Chevalley <pch@ap-i.net>
2014-07-30 11:31:39 +0000

    Install historic Mercury texture

--- install_data.sh
+++ install_data.sh
@@ -40,5 +40,6 @@ install -v -m 644 Database/Mars_Named_EN.csv $destdir/share/virtualplanet/Databa
 InstData VPA_Base_JPLeph $destdir
 InstData VPA_Base_Texture_Mars $destdir
 InstData VPA_Base_Texture_Mercury $destdir
+InstData VPA_Base_Texture_Mercury_Historical $destdir
 InstData VPA_Base_Texture_Venus $destdir
 
