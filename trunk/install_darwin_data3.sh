8b439065edd310418d5869bfd4f43e356593fcdf
pchev <pchev@f89c9d87-1aa7-4027-bf4b-a0fcb120b858>
2014-08-24 08:35:49 +0000

    add linux and mac installer

---
+++ trunk/install_darwin_data3.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# install data
+# must be installed after the software and data part
+
+function InstData {
+  pkg=$1.tgz
+  ddir=$2
+  tmpdir=$(mktemp -d -t tmp)
+  pkgz=BaseData/$pkg
+  if [ ! -e $pkgz ]; then
+     curl -L -o $pkgz http://sourceforge.net/projects/virtualplanet/files/9-Source_Data/$pkg/download
+  fi
+  tar xvzf $pkgz -C $tmpdir
+  cp -R -p $tmpdir/share/virtualplanet/* $ddir/
+  rm -rf $tmpdir/share/virtualplanet/*
+  rmdir $tmpdir/share/virtualplanet
+  rmdir $tmpdir/share
+  rmdir $tmpdir
+}
+
+destdir=$1
+
+if [ -z "$destdir" ]; then
+   export destdir=/tmp/virtualplanet
+fi
+
+echo Install virtualplanet data2 to $destdir
+
+install -m 755 -d $destdir
+
+# big data
+InstData VPA_Ext_Texture_Mercury $destdir 
+InstData VPA_Ext_Texture_Venus $destdir
+InstData VPA_Ext_Texture_Mars $destdir
