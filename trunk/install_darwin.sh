7fe75f479d05a99981462aad3ad41dbca43bcf79
pchev <pchev@f89c9d87-1aa7-4027-bf4b-a0fcb120b858>
2014-12-13 18:04:58 +0000

    fix Mac installer

--- trunk/install_darwin.sh
+++ trunk/install_darwin.sh
@@ -21,14 +21,14 @@ install -v -m 644 Installer/Mac/virtualplanet/virtualplanet.app/Contents/PkgInfo
 install -v -m 644 Installer/Mac/virtualplanet/virtualplanet.app/Contents/Resources/virtualplanet.icns $destdir/virtualplanet.app/Contents/Resources/
 
 install -v -m 755 -s vpa/vpa  $destdir/virtualplanet.app/Contents/MacOS/virtualplanet
-install -v -m 755 virtualplanet/library/plan404/libplan404.dylib  $destdir/libplan404.dylib
+install -v -m 755 vpa/library/plan404/libplan404.dylib  $destdir/libplan404.dylib
 install -v -m 644 Installer/Mac/virtualplanet/licence.txt $destdir/
 install -v -m 644 Installer/Mac/virtualplanet/readme.txt $destdir/
 
 # translation
 install -m 755 -d $destdir/language
-install -v -m 644 vpa/language/maplun.en.po $destdir/language/
-install -v -m 644 vpa/language/maplun.fr.po $destdir/language/
+install -v -m 644 vpa/language/vpa.en.po $destdir/language/
+install -v -m 644 vpa/language/vpa.fr.po $destdir/language/
 
 install -m 755 -d $destdir/data
 install -v -m 644 data/country.tab $destdir/data/
