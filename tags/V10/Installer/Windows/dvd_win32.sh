64bb6f6dd7e3ea840b3c07516623db2bfc0023c2
pchev <pchev@f89c9d87-1aa7-4027-bf4b-a0fcb120b858>
2015-08-26 15:37:58 +0000

    wrong version

--- tags/V10/Installer/Windows/dvd_win32.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir tmp
-cp virtualplanet*windows.exe tmp/
-cp /home/pch/src/vpa/build/Installer/Windows/DVD/* tmp/
-
-mkisofs -R -r -l -J -quiet -Vvirtualplanet -ovirtualplanet-1.0-windows.iso tmp
-
-#rm -rf tmp
--- tags/V10/trunk/textures/Mars/Historical/Schiaparelli_1886/mkslice.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export MAGICK_MEMORY_LIMIT=3000000000
-export MAGICK_MAP_LIMIT=2000000000
-export MAGICK_AREA_LIMIT=2000000000
-
-echo L1
-mkdir L1
-convert Schiaparelli_1886_L1.jpg +gravity -crop 1000x1000 -bordercolor white -border 12x12 -quality 65% L1/%d.jpg
