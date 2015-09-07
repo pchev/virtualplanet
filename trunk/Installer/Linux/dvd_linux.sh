7d196347d1561df63b6db1f32976fcca31b63571
pchev <pchev@f89c9d87-1aa7-4027-bf4b-a0fcb120b858>
2015-09-07 13:11:34 +0000

    update dvd script

--- trunk/Installer/Linux/dvd_linux.sh
+++ trunk/Installer/Linux/dvd_linux.sh
@@ -6,8 +6,8 @@ mkdir tmp
 mkdir tmp/DEB
 mkdir tmp/RPM
 
-cp virtualplanet*deb tmp/DEB/
-cp virtualplanet*rpm tmp/RPM/
+cp Linux/deb/virtualplanet*deb tmp/DEB/
+cp Linux/rpm/virtualplanet*rpm tmp/RPM/
 cp /home/pch/src/vpa/Installer/Linux/DVD/* tmp/
 
 mkisofs -R -r -l -J -quiet -Vvirtualplanet -ovirtualplanet-1.0-linux.iso tmp
