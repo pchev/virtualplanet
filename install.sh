a837692edfc5aaa350a7ab6e1ce9f9eb442d94d9
Patrick Chevalley <pch@ap-i.net>
2015-12-17 10:09:39 +0000

    Add missing icons

--- install.sh
+++ install.sh
@@ -19,10 +19,16 @@ install -m 755 -d $destdir/share/applications
 install -m 755 -d $destdir/share/doc
 install -m 755 -d $destdir/share/doc/virtualplanet
 install -m 755 -d $destdir/share/pixmaps
+install -m 755 -d $destdir/share/icons
+install -m 755 -d $destdir/share/icons/hicolor
+install -m 755 -d $destdir/share/icons/hicolor/48x48
+install -m 755 -d $destdir/share/icons/hicolor/48x48/apps
+
 install -v -m 755 -s vpa/vpa  $destdir/bin/virtualplanet
 install -v -m 644 -s vpa/library/plan404/libvpa404.so  $destdir/lib/libvpa404.so
 install -v -m 644 Installer/Linux/vpa/share/applications/virtualplanet.desktop $destdir/share/applications/virtualplanet.desktop
 install -v -m 644 Installer/Linux/vpa/share/pixmaps/virtualplanet.png $destdir/share/pixmaps/virtualplanet.png
+install -v -m 644 Installer/Linux/vpa/share/pixmaps/virtualplanet.png $destdir/share/icons/hicolor/48x48/apps/virtualplanet.png
 install -v -m 644 Installer/Linux/vpa/share/doc/virtualplanet/changelog $destdir/share/doc/virtualplanet/changelog
 install -v -m 644 Installer/Linux/vpa/share/doc/virtualplanet/copyright $destdir/share/doc/virtualplanet/copyright
 
