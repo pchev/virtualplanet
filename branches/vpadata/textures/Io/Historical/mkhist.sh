998ffb1aca4f96a2672851ecb9f9b7d5e1961f36
pchev <pchev@f89c9d87-1aa7-4027-bf4b-a0fcb120b858>
2016-11-03 13:00:45 +0000

    make historic textures

---
+++ branches/vpadata/textures/Io/Historical/mkhist.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+     
+rm -rf share
+mkdir -p share/virtualplanet/Textures/Io/Historical
+
+ls -d * | grep -v mkhist | grep -v share | while read d 
+  do
+    echo $d
+    cd $d
+    convert $d.jpg -resize 4000x2000 l1.png
+    mkdir -p ../share/virtualplanet/Textures/Io/Historical/$d/L1
+    convert l1.png +gravity -crop 1000x1000 -bordercolor white -border 12x12 -quality 65% ../share/virtualplanet/Textures/Io/Historical/$d/L1/%d.jpg
+    rm l1.png
+    cd ..
+  done
+
+  tar cvzf VPA_Base_Texture_Io_Historical.tgz share
+
+  
---
+++ branches/vpadata/textures/Jupiter/Historical/mkhist.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+     
+rm -rf share
+mkdir -p share/virtualplanet/Textures/Jupiter/Historical
+
+ls -d * | grep -v mkhist | grep -v share | while read d 
+  do
+    echo $d
+    cd $d
+    convert $d.jpg -resize 4000x2000 l1.png
+    mkdir -p ../share/virtualplanet/Textures/Jupiter/Historical/$d/L1
+    convert l1.png +gravity -crop 1000x1000 -bordercolor white -border 12x12 -quality 65% ../share/virtualplanet/Textures/Jupiter/Historical/$d/L1/%d.jpg
+    rm l1.png
+    cd ..
+  done
+
+tar cvzf VPA_Base_Texture_Jupiter_Historical.tgz share
